f5bb0811dfa560e50a1594ddd2ce6dc2ef8bde83
kitsdream <79633060+kitsdream@users.noreply.github.com>
2023-10-21 14:25:28 +0800

    Update ResetBlockScale_cmd.py

--- scripts/ResetBlockScale/ResetBlockScale_cmd.py
+++ scripts/ResetBlockScale/ResetBlockScale_cmd.py
@@ -2,10 +2,10 @@
 @name:          ResetBlockScale
 @description:   Resets the scale of a block, keeping the rotation around the insertion point.
 @author:        Ejnar Brendsdal
-@version:       1.3
+@version:       1.3cn
 @attribution:   Inspired by the ResetBlock script by Dale Fugier. Thank you.
 @link:          https://github.com/ejnaren/rhinotools
-@notes:         Works with Rhino 5.
+@notes:         Works with Rhino 7.
 
 @license:
 
@@ -20,7 +20,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-@Installation: Copy to the Rhino script folder. ie.: C:\Users\"USER"\AppData\Roaming\McNeel\Rhinoceros\5.0\scripts
+@Installation: Copy to the Rhino script folder. ie.: C:\Users\"USER"\AppData\Roaming\McNeel\Rhinoceros\7.0\scripts
                 Options to use the function:
                 1. Recommended: Import the bundled "Block Tools" toolbar with readymade buttons to call the functions.
                 2. Add a new button with the following macro: ( _NoEcho !-_RunPythonScript "ResetBlockScale.py" _Echo )
@@ -36,6 +36,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #******* Imports ********************
 #************************************
 
+#coding=utf-8
+
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
 import Rhino.Geometry as G
@@ -45,16 +47,16 @@ import Rhino.Geometry as G
 
 def RunCommand( is_interactive ):
     if sc.escape_test(False):
-        print "script cancelled" #do something
+        print "操作已取消" #do something
 
-    print "Resetting..."
+    print "正在重置中"
 
     #******* Get blocks ***********''****
     #************************************
 
-    objectIds = rs.GetObjects("Pick some blocks", 4096, preselect=True)
+    objectIds = rs.GetObjects("选取要操作的图块", 4096, preselect=True)
     if not objectIds:
-        print "No objects"
+        print "没有选取物件"
         return False
 
     rs.EnableRedraw(False)
@@ -104,7 +106,7 @@ def RunCommand( is_interactive ):
 
     rs.EnableRedraw(True)
 
-    print "...aaand its done."
+    print "操作已完成"
     #End RunCommand()
 
     #end sane
